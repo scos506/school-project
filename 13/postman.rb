b824e639cfbb403979d403d90a90d51eb6ce5d2a
scos506 <scos506@gmail.com>
2020-01-22 05:36:18 +0000

    finished experiment

--- 13/postman.rb
+++ 13/postman.rb
@@ -3,7 +3,7 @@
 require 'httparty'
 require 'json'
 
-api_key = "PMAK-5e27b93f7cce8f0031c6a037-2620be2af1814805011d91426c70acd282"
+api_key = "!234"
 
 response = HTTParty.get("https://api.psotman.com/do/something",
                         :headers => {
