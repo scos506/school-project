8409366577f62983102f059507d79cddd93e47cf
scos506 <scos506@gmail.com>
2020-01-23 01:50:23 +0000

    trying out an invoicing strategy

---
+++ 15/payment.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+pub_key = "pk_test_B6Kk8gdH0E2Rorzid5qB6t98000VO2KVtw"
+sec_key = "sk_test_PeUmONAQKA5tfMVEJ2sjl3vI00A5fTgvfp"
+
+s = Stripe.new(pub_key, sec_key)
+
+s.invoices.each do |inv|
+  puts inv.name
+  puts inv.amount
+end
