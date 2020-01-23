3bad198fe9c6c3106674dcb8671c26a22f090679
scos506 <scos506@gmail.com>
2020-01-23 02:11:34 +0000

    finished experiment

--- 15/payment.rb
+++ 15/payment.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-pub_key = "pk_test_B6Kk8gdH0E2Rorzid5qB6t98000VO2KVtw"
-sec_key = "sk_test_PeUmONAQKA5tfMVEJ2sjl3vI00A5fTgvfp"
+pub_key = "pk_test_1234"
+sec_key = "sk_test_1234"
 
-prod_keys = "pk_live_B6Kk8gdH0E2Rorzid5qB6t98000VO2KVtw"
-prod_sec = "sk_live_PeUmONAQKA5tfMVEJ2sjl3vI00A5fTgvfp"
+prod_keys = "pk_live_1234"
+prod_sec = "sk_live_1234"
 
 s = Stripe.new(pub_key, sec_key)
 
