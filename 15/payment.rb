cd12207fda25b448339f28471575d3c06892c62c
scos506 <scos506@gmail.com>
2020-01-23 01:58:13 +0000

    added prod keys

--- 15/payment.rb
+++ 15/payment.rb
@@ -3,6 +3,9 @@
 pub_key = "pk_test_B6Kk8gdH0E2Rorzid5qB6t98000VO2KVtw"
 sec_key = "sk_test_PeUmONAQKA5tfMVEJ2sjl3vI00A5fTgvfp"
 
+prod_keys = "pk_live_B6Kk8gdH0E2Rorzid5qB6t98000VO2KVtw"
+prod_sec = "sk_live_PeUmONAQKA5tfMVEJ2sjl3vI00A5fTgvfp"
+
 s = Stripe.new(pub_key, sec_key)
 
 s.invoices.each do |inv|
